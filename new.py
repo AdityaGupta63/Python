af918b6d80386bddfd434b9e0fc8bbc577036e8b
Aditya <modanwaladicode@gmail.com>
2023-08-02 22:58:20 +0530

    valiodation method

---
+++ new.py
@@ -0,0 +1,10 @@
+user = input('Enter here : ')
+
+if user.isnumeric():
+    print('you enter a number')
+if user.isalpha():
+    print('you enter a string')
+if user.isalnum():
+    print('you enter a alpha numeric')
+if user.islower():
+    print('you enter a lower case string')
