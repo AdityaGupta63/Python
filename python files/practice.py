0030c625019a5c347f5796fd24b0a1229b8672a2
Aditya <modanwaladicode@gmail.com>
2023-08-16 00:46:18 +0530

    password generator

---
+++ python files/practice.py
@@ -0,0 +1,16 @@
+# ----Take a string input and print it's length.----
+# user = input('Enter your name here : ')
+# print(len(user))
+
+# ----Print the last word of the string Python is great using slices.----
+# str = 'Python is great'
+# newstr = str.split()
+# print(newstr[-1])   
+
+# ----Print the each word in different line of string python is everywhere.----
+# str = 'python is everywhere'
+# for i in str:
+#     print(i)
+
+# ----Print the string Hello World! in reverse.----
+str = 'Hello world!'
