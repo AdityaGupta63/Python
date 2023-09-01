ea15b123c6374f4bbd2fc07b950858af59d68b94
Aditya <modanwaladicode@gmail.com>
2023-09-01 22:40:20 +0530

    calculator

--- python mini project/calculator.py
+++ python mini project/calculator.py
@@ -1,14 +1,16 @@
+print('--------Welcome to Calculator----------------')
+
 num1 = int(input(('Enter num 1 here : ')))
 opt = input('Enter operator here : ')
 num2 = int(input('Enter num2 here : '))
 
 if opt == '+':
-    print(f'{num1} + {num2} : {num1 + num2}')
+    print(f'{num1} + {num2} = {num1 + num2}')
 elif opt == '-':
-    print(f'{num1} - {num2} : {num1 - num2}')
+    print(f'{num1} - {num2} = {num1 - num2}')
 elif opt == '*':
-    print(f'{num1} - {num2} : {num1 * num2}')
+    print(f'{num1} * {num2} = {num1 * num2}')
 elif opt == '/':
-    print(f'{num1} * {num2} : {num1 / num2}')
+    print(f'{num1} / {num2} = {num1 / num2}')
 else:
     print('Invalid input')
