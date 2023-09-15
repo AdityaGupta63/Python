829aede33aa0308fb74354542aca6ebb34ce0595
Aditya <modanwaladicode@gmail.com>
2023-09-15 23:11:21 +0530

    streamlit

--- projects/ne2/scraper.py
+++ projects/ne2/scraper.py
@@ -1,5 +1,27 @@
-import streamlit as st
-import requests as r
 from bs4 import BeautifulSoup
+import requests as r
 
-st.markdown('<h1 padding-left: 10px;>Aditya</h1>')
+url = 'https://www.flipkart.com/search?q=smart%20watch&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
+try:
+    response = r.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print('sucess')
+except Exception as e:
+    print(e)
+
+target = soup.find('div', class_ = '_1YokD2 _3Mn1Gg')
+if target:
+    print("yes")
+else:
+    print('no')
+
+products = target.find_all('div', class_ = '_1AtVbE col-12-12')
+if products:
+    print("yes product found!")
+    total = len(products)
+    print (f'{total} products are there.')
+else:
+    print("No Product Found!!")
+
+# for item in products:
+#     try: title = item.find
--- projects/web scraping amazon/app.py
+++ projects/web scraping amazon/app.py
@@ -3,19 +3,24 @@ import requests
 from bs4 import BeautifulSoup
 
 st.set_page_config(
-    page_title='Amazon Web Scraping'
+    page_title='Flipkart Web Scraping'
 )
+url = 'https://www.snapdeal.com/search?keyword=smart%20watch&sort=rlvncy'
+Header = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+link = requests.get(url, headers=Header)
 
 st.title('Amazon Web Scraping Project !')
 st.subheader('A program that extracts product information, reviews and rating from Amazon.com .')
 
 
-url = 'https://www.amazon.in/Apple-Cellular-Rugged-Titanium-Orange/dp/B0BDKGNMJQ?ref_=ast_sto_dp&th=1'
-
-Header = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
-
-link = requests.get(url, headers=Header)
 
 soup = BeautifulSoup(link.content, 'html.parser')
 
-st.title(soup.find_all(id='productTitle')).title
+target = soup.find('div', class_ = 'product-row js-product-list centerCardAfterLoadWidgets dp-click-widgets')
+# if target:
+#     st.markdown('found')
+# else:
+#     st.markdown('not found')
+
+product = target.find_all('div', class_ = 'js-section clearfix dp-widget dp-fired')
+st.markdown(product)
